c1e209e4ae1e0dc5a58e496f1ccbb50ed0153502
Jaqueline Peixer <peixer.jaqueline@gmail.com>
2023-03-25 16:29:34 -0300

    Aula 02 - Ignite Timer

--- 02-aula-ignite-timer/src/@types/styled.d.ts
+++ 02-aula-ignite-timer/src/@types/styled.d.ts
@@ -4,5 +4,5 @@ import { defaultTheme } from '../styles/themes/default';
 type ThemeType = typeof defaultTheme;
 
 declare module 'styled-components' {
-    export interface defaultTheme extends ThemeType {}
+    export interface DefaultTheme extends ThemeType {}
 }
