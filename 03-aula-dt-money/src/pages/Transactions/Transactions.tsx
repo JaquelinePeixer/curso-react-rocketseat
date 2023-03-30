b05b7562508188552cc8f125112b4a7abf762bb2
Jaqueline Peixer <peixer.jaqueline@gmail.com>
2023-03-30 07:44:08 -0300

    Aula 03 - sumarry e criação da tabela

--- 03-aula-dt-money/src/pages/Transactions/Transactions.tsx
+++ 03-aula-dt-money/src/pages/Transactions/Transactions.tsx
@@ -1,10 +1,40 @@
 import { Header } from "../../components/Header/Header";
+import { Summary } from "../../components/Summary/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 
 export function Transactions() {
     return (
         <>
             <Header />
+            <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td>Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Vendas</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td>Hamburguer</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    - R$ 59,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </>
     )
 }
